b41b4d2b41c21244bf07be1d5da6f2ed15a64ef4
Ryan Barner <emailryanbarner@gmail.com>
2019-05-21 04:48:13 +0000

    Turning SQLAlchemy's Track Modifications off

--- model.py
+++ model.py
@@ -67,7 +67,8 @@ class Rating(db.Model):
 
 def connect_to_db(app):
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///petvenues'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///petvenues"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
